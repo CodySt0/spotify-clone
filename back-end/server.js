8982ea06aa1721cb78d61b3ca0863a8a58d2ae53
CodySt0 <Codistgeorge@gmail.com>
2024-08-28 14:02:08 -0700

    connect to mongoDB and cloudinary

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import songRouter from "./src/routes/songRoute.js";
+import connectDB from "./src/config/mongodb.js";
+import connectCloudinary from "./src/config/cloudinary.js";
 
 //app config
 const app = express();
 const port = process.env.port || 5002;
+connectDB();
+connectCloudinary;
 
 // middlewares
 app.use(express.json());
@@ -12,6 +17,8 @@ app.use(express.json());
 app.use(cors());
 
 //rotues
+
+app.use("/api/song", songRouter);
 
 app.get("/", (req, res) => res.send("API WORKING"));
 
