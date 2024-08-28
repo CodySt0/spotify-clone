0ff8f0d94d0e85db526af5ff53b804d6c2a12f45
CodySt0 <Codistgeorge@gmail.com>
2024-08-28 01:37:04 -0700

    worked on navbar

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -4,7 +4,7 @@ import DisplayHome from "./DisplayHome";
 
 const Display = () => {
   return (
-    <div className="w-[100% m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto">
+    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto">
       <Routes>
         <Route path="/" element={<DisplayHome />} />
       </Routes>
