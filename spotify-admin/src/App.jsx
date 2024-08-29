a65559890c890120348702b3cd2569b1137c32f0
CodySt0 <Codistgeorge@gmail.com>
2024-08-28 18:10:07 -0700

    styling

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
 import { Routes, Route } from "react-router-dom";
 import AddSong from "./pages/AddSong";
 import AddAlbum from "./pages/AddAlbum";
